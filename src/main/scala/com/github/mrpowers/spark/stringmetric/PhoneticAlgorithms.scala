77d5371acb57988249dbf6328c0401b12e371b63
MrPowers <matthewkevinpowers@gmail.com>
2017-09-12 06:29:10 -0500

    Add the nysiis and refined_soundex phonetic algorithms

--- src/main/scala/com/github/mrpowers/spark/stringmetric/PhoneticAlgorithms.scala
+++ src/main/scala/com/github/mrpowers/spark/stringmetric/PhoneticAlgorithms.scala
@@ -1,8 +1,7 @@
 package com.github.mrpowers.spark.stringmetric
 
 import org.apache.spark.sql.functions._
-
-import org.apache.commons.codec.language.DoubleMetaphone
+import org.apache.commons.codec.language.{DoubleMetaphone, Nysiis, RefinedSoundex}
 
 object PhoneticAlgorithms {
 
@@ -13,6 +12,22 @@ object PhoneticAlgorithms {
     if (str1.equals("")) return None
     val dm = new DoubleMetaphone()
     Some(dm.encode(str1))
+  }
+
+  val nysiis = udf[Option[String], String](nysiisFun)
+
+  def nysiisFun(s1: String): Option[String] = {
+    val str1 = Option(s1).getOrElse(return None)
+    val ny = new Nysiis()
+    Some(ny.encode(str1))
+  }
+
+  val refined_soundex = udf[Option[String], String](refinedSoundexFun)
+
+  def refinedSoundexFun(s1: String): Option[String] = {
+    val str1 = Option(s1).getOrElse(return None)
+    val rs = new RefinedSoundex()
+    Some(rs.encode(str1))
   }
 
 }
--- src/test/scala/com/github/mrpowers/spark/stringmetric/PhoneticAlgorithmsSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/stringmetric/PhoneticAlgorithmsSpec.scala
@@ -49,4 +49,80 @@ class PhoneticAlgorithmsSpec
 
   }
 
+  describe("nysiis") {
+
+    it("encodes a string using the nysiis algorithm") {
+
+      val sourceDF = spark.createDF(
+        List(
+          ("night"),
+          ("cat"),
+          (""),
+          (null)
+        ), List(
+          ("word1", StringType, true)
+        )
+      )
+
+      val actualDF = sourceDF.withColumn(
+        "word1_nysiis",
+        PhoneticAlgorithms.nysiis(col("word1"))
+      )
+
+      val expectedDF = spark.createDF(
+        List(
+          ("night", "NAGT"),
+          ("cat", "CAT"),
+          ("", ""),
+          (null, null)
+        ), List(
+          ("word1", StringType, true),
+          ("word1_nysiis", StringType, true)
+        )
+      )
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+  }
+
+  describe("refined_soundex") {
+
+    it("encodes a string with the refined soundex algorithm") {
+
+      val sourceDF = spark.createDF(
+        List(
+          ("night"),
+          ("cat"),
+          (""),
+          (null)
+        ), List(
+          ("word1", StringType, true)
+        )
+      )
+
+      val actualDF = sourceDF.withColumn(
+        "word1_refined_soundex",
+        PhoneticAlgorithms.refined_soundex(col("word1"))
+      )
+
+      val expectedDF = spark.createDF(
+        List(
+          ("night", "N80406"),
+          ("cat", "C306"),
+          ("", ""),
+          (null, null)
+        ), List(
+          ("word1", StringType, true),
+          ("word1_refined_soundex", StringType, true)
+        )
+      )
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+  }
+
 }
