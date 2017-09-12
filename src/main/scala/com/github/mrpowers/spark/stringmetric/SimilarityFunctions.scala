4a27dfeb2927df5453507ed777acff6344165b0b
MrPowers <matthewkevinpowers@gmail.com>
2017-09-12 05:06:47 -0500

    Use org.apache.commons.text.similarity to calculate the HammingDistance

--- src/main/scala/com/github/mrpowers/spark/stringmetric/SimilarityFunctions.scala
+++ src/main/scala/com/github/mrpowers/spark/stringmetric/SimilarityFunctions.scala
@@ -4,7 +4,7 @@ import java.util.Locale
 
 import com.rockymadden.stringmetric.similarity._
 import org.apache.spark.sql.functions._
-import org.apache.commons.text.similarity.{CosineDistance, FuzzyScore}
+import org.apache.commons.text.similarity.{CosineDistance, FuzzyScore, HammingDistance}
 
 object SimilarityFunctions {
 
@@ -39,7 +39,8 @@ object SimilarityFunctions {
   def hammingFun(s1: String, s2: String): Option[Int] = {
     val str1 = Option(s1).getOrElse(return None)
     val str2 = Option(s2).getOrElse(return None)
-    HammingMetric.compare(str1, str2)
+    val h = new HammingDistance()
+    Some(h.apply(str1, str2))
   }
 
   val jaccard = udf[Option[Double], String, String, Int](jaccardFun)
