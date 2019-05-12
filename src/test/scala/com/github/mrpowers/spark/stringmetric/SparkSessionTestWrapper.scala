0d4c89a819bec92d9dce335840264b29770af99c
MrPowers <matthewkevinpowers@gmail.com>
2019-05-12 13:57:40 -0500

    set shuffle partitions to one in the test suite

--- src/test/scala/com/github/mrpowers/spark/stringmetric/SparkSessionTestWrapper.scala
+++ src/test/scala/com/github/mrpowers/spark/stringmetric/SparkSessionTestWrapper.scala
@@ -5,7 +5,12 @@ import org.apache.spark.sql.SparkSession
 trait SparkSessionTestWrapper {
 
   lazy val spark: SparkSession = {
-    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("spark session")
+      .config("spark.sql.shuffle.partitions", "1")
+      .getOrCreate()
   }
 
 }
