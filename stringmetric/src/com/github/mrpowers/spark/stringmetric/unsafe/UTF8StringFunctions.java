ffd7d5804281321a47ae9e422b3c4d2ed39503f0
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2019-05-29 22:38:05 -0400

    Ensure hamming is only used on same length strings

--- stringmetric/src/com/github/mrpowers/spark/stringmetric/unsafe/UTF8StringFunctions.java
+++ stringmetric/src/com/github/mrpowers/spark/stringmetric/unsafe/UTF8StringFunctions.java
@@ -5,9 +5,16 @@ import org.apache.spark.unsafe.types.UTF8String;
 public class UTF8StringFunctions {
     // Adopted from org.apache.commons.text.similarity.HammingDistance
     public static int hammingDistance(UTF8String left, UTF8String right) {
+        if (left.numChars() != right.numChars()) {
+            throw new java.lang.IllegalArgumentException(
+                "Hamming distance is only defined for strings of same length!"
+            )
+        }
+
         int distance = 0;
         byte[] leftBytes = left.getBytes();
         byte[] rightBytes = right.getBytes();
+
         for (int i = 0; i < left.numBytes(); i++) {
             if (leftBytes[i] != rightBytes[i]) {
                 distance++;
