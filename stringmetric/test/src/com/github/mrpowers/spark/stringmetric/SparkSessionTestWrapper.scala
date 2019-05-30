ddbe9d32e2814be20ca2b04bf7f17941d214f6e9
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2019-05-29 23:03:33 -0400

    Hide spark info in tests

--- stringmetric/test/src/com/github/mrpowers/spark/stringmetric/SparkSessionTestWrapper.scala
+++ stringmetric/test/src/com/github/mrpowers/spark/stringmetric/SparkSessionTestWrapper.scala
@@ -1,10 +1,12 @@
 package com.github.mrpowers.spark.stringmetric
 
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Logger, Level}
 
 trait SparkSessionTestWrapper {
 
   lazy val spark: SparkSession = {
+    Logger.getLogger("org").setLevel(Level.OFF)
     SparkSession
       .builder()
       .master("local")
